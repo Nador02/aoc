820777c9d6991a686cce5bf785b565fc7f82f0a3
Nathan Rand <nsrand02@gmail.com>
2024-08-04 16:39:44 -0500

    Day 3 problem 1, done :D

--- 2023/day_03/problem1.py
+++ 2023/day_03/problem1.py
@@ -8,7 +8,7 @@ Date: 08.03.2024
 import regex as re  
 
 _PART_NUMBER_PATTERN = re.compile(r'\d{1,3}')
-_SYMBOL_LIST = [r"#", r"/", r"@", r"\$", r"=", r"\+", r"%", r"\*", r"\&"]
+_SYMBOL_PATTERN = r'[^\w\s.]+'
 
 def main():
     with open("input.txt", "r") as f:
@@ -17,26 +17,25 @@ def main():
         for i in range(len(schematic)):
             part_numbers_list = _PART_NUMBER_PATTERN.findall(schematic[i])
             part_numbers_positions = [(m.start(0), m.end(0)) for m in re.finditer(_PART_NUMBER_PATTERN, schematic[i])]
-            part_numbers = {int(part_numbers_list[i]) : part_numbers_positions[i] for i in range(len(part_numbers_list))}
+            part_numbers = [(int(part_numbers_list[i]), part_numbers_positions[i]) for i in range(len(part_numbers_list))]
             symbol_positions = []
-            for symbol in _SYMBOL_LIST:
-                if i != 0:
-                    symbol_positions.extend([m.start() for m in re.finditer(re.compile(symbol), schematic[i-1])])
-                if i != len(schematic)-1:
-                    symbol_positions.extend([m.start() for m in re.finditer(re.compile(symbol), schematic[i+1])])
-                symbol_positions.extend([m.start() for m in re.finditer(re.compile(symbol), schematic[i])])
+            if i != 0:
+                symbol_positions.extend([m.start() for m in re.finditer(_SYMBOL_PATTERN, schematic[i-1])])
+            if i != len(schematic)-1:
+                symbol_positions.extend([m.start() for m in re.finditer(_SYMBOL_PATTERN, schematic[i+1])])
+            symbol_positions.extend([m.start() for m in re.finditer(_SYMBOL_PATTERN, schematic[i])])
                 
-            for part_number, positions in part_numbers.items():
+            for part_number, positions in part_numbers:
                 if check_if_symbol_near_part_number(positions, symbol_positions):
                     part_number_sum += part_number
                 
         print(f"Sum of all part numbers in engine schematic: {part_number_sum}")
 
 def check_if_symbol_near_part_number(positions, symbol_positions):
-    positional_range = range(positions[0]-1,positions[1]+2)
+    positional_range = range(positions[0]-1,positions[1]+1)
     for spot in positional_range:
         if spot in symbol_positions:
             return True
 
 if __name__ == "__main__":
-    main()
+    main() 
