8460333e75ce471d462f6be805a801ff52fdee24
Nathan Rand <nsrand02@gmail.com>
2024-08-13 20:48:58 -0500

    Day 7 problem 1

--- 2023/day_07/problem1.py
+++ 2023/day_07/problem1.py
@@ -5,9 +5,10 @@ Problem: 01
 Author: Nathan Rand
 Date: 08.11.2024
 """
-from enum import Enum
+from enum import IntEnum
+from typing import List
 
-class HandPowerRanking(Enum):
+class HandPowerRanking(IntEnum):
     FIVEOFAKIND = 7
     FOUROFAKIND = 6
     FULLHOUSE = 5
@@ -58,11 +59,46 @@ class CamelHand():
             return HandPowerRanking.ONEPAIR
         return HandPowerRanking.HIGHCARD
 
-def compare_camel_hands(camel_hand_1 : CamelHand, camel_hand_2: CamelHand):
-    print("Test for now")
+    def compare(self, other_camel_hand: "CamelHand"):
+        """Compare two camel hands.
+        Returns true if greater than, false otherwise.
+        """
+        if self.hand_type != other_camel_hand.hand_type:
+            return self.hand_type > other_camel_hand.hand_type
+        
+        # Now go through the cards in each hand to compare
+        for i in range(len(self.hand)):
+            # Check if our cards are the same, if so, continue
+            # to find a difference on a later card
+            if self.hand[i] == other_camel_hand.hand[i]:
+                continue
+            
+            # Convert our face cards based on the defined dictionary,
+            # if not a face card, convert to an int directly
+            this_card = int(self.hand[i]) if self.hand[i].isnumeric() else _FACE_CARD_VALUES[self.hand[i]]
+            other_card = int(other_camel_hand.hand[i]) if other_camel_hand.hand[i].isnumeric() else _FACE_CARD_VALUES[other_camel_hand.hand[i]]
+
+            return this_card > other_card
+        
+        # If for some reason we have hands that are equal
+        # (we shouldn't, but idfk) return None
+        return None
+
+def camel_quicksort(hands: List[CamelHand]):
+    """Applies quicksort based on the CamelHand.compare()
+    functionality. Sorts from least to greatest.
+    """
+    if len(hands) <= 1:
+        return hands
+
+    pivot = hands[0]
+    left = [hand for hand in hands[1:] if pivot.compare(hand)]
+    right = [hand for hand in hands[1:] if hand.compare(pivot)]
+    return camel_quicksort(left) + [pivot] + camel_quicksort(right)
+        
 
 def main():
-    with open("example.txt", "r") as f:
+    with open("input.txt", "r") as f:
         # Extract our times and distance data
         card_hand_strings = f.read().split("\n")
         
@@ -70,10 +106,18 @@ def main():
         camel_hands = []
         for card_hand_string in card_hand_strings:
             hand, bid = card_hand_string.split(" ")
-            camel_hands.append(CamelHand(hand, bid))
+            camel_hands.append(CamelHand(hand, int(bid)))
         
-        # Binary search for our insert location
+        # Apply our custom quicksort to sort our camel hands
+        sorted_camel_hands = camel_quicksort(camel_hands)
 
+        # Compute the total winnings
+        total_winnings = 0
+        for i in range(len(sorted_camel_hands)):
+            total_winnings += sorted_camel_hands[i].bid*(i+1)
+        
+        # Output our result
+        print(f"Total Winnings of the set of Camel Card Hands: {total_winnings}")
 
 if __name__ == "__main__":
     main() 
