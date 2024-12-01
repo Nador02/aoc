87e1575ee5578cb0ee1209637c1f6e6d0cd1d1b5
Nathan Rand <nsrand02@gmail.com>
2024-11-30 20:44:40 -0600

    Setup for day 1 of AoC 2024! Yay! So excited :)

---
+++ 2024/day_01/problem1.py
@@ -0,0 +1,15 @@
+"""
+Advent of Code 2024
+Day: 01
+Problem: 01
+Author: Nathan Rand
+Date: 12.01.2024
+"""
+
+def main():
+    with open("example.txt", "r") as f:
+        lines = f.read().split("\n")
+    print(lines)
+    
+if __name__ == "__main__":
+    main() 
