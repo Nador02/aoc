571672ff81f30f8a548dffe5095868a162ec05b7
Nathan Rand <nsrand02@gmail.com>
2024-12-01 20:13:08 -0600

    Template setup and laptop sync for day2

---
+++ 2024/day_02/problem1.py
@@ -0,0 +1,15 @@
+"""
+Advent of Code 2024
+Day: 02
+Problem: 02
+Author: Nathan Rand
+Date: 12.02.2024
+"""
+
+
+def main():
+    """Advent of Code - Day 02 - Part 01 [Insert Problem Name Here]"""
+
+
+if __name__ == "__main__":
+    main()
