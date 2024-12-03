a0dc646432dc4a8dc5cea0594793640f3ca29517
Nathan Rand <nsrand02@gmail.com>
2024-12-03 08:05:00 -0600

    Day 3 prob1  setup

---
+++ 2024/day_03/problem1.py
@@ -0,0 +1,19 @@
+"""
+Advent of Code 2024
+Day: 03
+Problem: 01
+Author: Nathan Rand
+Date: 12.03.2024
+"""
+_INPUT_FILE_NAME = "input.txt"
+
+
+def main():
+    """Advent of Code - Day 03 - Part 01 [Mull It Over]"""
+    # Read in our reports
+    with open(_INPUT_FILE_NAME, "r") as f:
+        report_data = f.read()
+
+
+if __name__ == "__main__":
+    main()
