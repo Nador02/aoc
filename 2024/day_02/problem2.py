ebac55f408cb18ca3f7c3ddc3b15ddf4a02c22e9
Nathan Rand <nsrand02@gmail.com>
2024-12-03 07:52:33 -0600

    Day 2 problem2 solved

--- 2024/day_02/problem2.py
+++ 2024/day_02/problem2.py
@@ -1,7 +1,7 @@
 """
 Advent of Code 2024
 Day: 02
-Problem: 01
+Problem: 02
 Author: Nathan Rand
 Date: 12.02.2024
 """
@@ -20,8 +20,27 @@ def is_report_safe(report: List[float]):
     return is_monotonic and np.all(abs(report_diff) <= 3)
 
 
+def is_report_safe_w_problem_dampener(report: List[float]):
+    # Check if our report is safe without the dampener
+    if is_report_safe(report):
+        return True
+
+    # NOTE: Brute Force, can improve
+    # If not safe, apply the dampener by removing each element
+    # in order and checking if removing any will allow for a safe report.
+    for i in range(len(report)):
+        trimmed_report = (
+            report[:i] + report[i+1:]
+            if i < len(report)-1 else report[:-1]
+        )
+        if is_report_safe(trimmed_report):
+            return True
+
+    return False
+
+
 def main():
-    """Advent of Code - Day 02 - Part 01 [Red-Nosed Reports]"""
+    """Advent of Code - Day 02 - Part 02 [Red-Nosed Reports]"""
     # Read in our reports
     with open(_INPUT_FILE_NAME, "r") as f:
         report_data = f.read().split("\n")
@@ -31,13 +50,13 @@ def main():
     # Determine how many reports are safe
     safe_reports = 0
     for report in fission_reports:
-        if is_report_safe(report):
+        if is_report_safe_w_problem_dampener(report):
             safe_reports += 1
 
     # Output our result
     print(
-        "The number of safe rudolf nuclear fission reports is: "
-        f"{int(safe_reports)} reports"
+        "The number of safe rudolf nuclear fission reports "
+        f"with the problem dampener is: {int(safe_reports)} reports"
     )
 
 
