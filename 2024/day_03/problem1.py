c82fc698d27865967bdaf795d78be2ea0843e2dc
Nathan Rand <nsrand02@gmail.com>
2024-12-03 08:15:06 -0600

    Day 3 problem1 solved

--- 2024/day_03/problem1.py
+++ 2024/day_03/problem1.py
@@ -5,14 +5,29 @@ Problem: 01
 Author: Nathan Rand
 Date: 12.03.2024
 """
+import re
 _INPUT_FILE_NAME = "input.txt"
 
 
 def main():
     """Advent of Code - Day 03 - Part 01 [Mull It Over]"""
-    # Read in our reports
+    # Read in our corrupted memory data
     with open(_INPUT_FILE_NAME, "r") as f:
-        report_data = f.read()
+        corrupted_memory = f.read()
+
+    # Get all matches for our given regex pattern
+    matches = re.findall(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)", corrupted_memory)
+
+    # Sum the matching multiplication instructions
+    mul_instructions_sum = 0
+    for match in matches:
+        mul_instructions_sum += int(match[0])*int(match[1])
+
+    # Output our result
+    print(
+        "The sum of all valid multiplication instructions in the "
+        f"corrupted memory is: {mul_instructions_sum}"
+    )
 
 
 if __name__ == "__main__":
