5b3f68b8ecd966142ae0181a76ce91c355a59687
Nathan Rand <nsrand02@gmail.com>
2024-12-04 19:52:39 -0600

    Added template directory for creating next problem day stuff, along with day 5 setup

---
+++ 2024/day_05/problem1.py
@@ -0,0 +1,18 @@
+"""
+Advent of Code 2024
+Day: 05
+Problem: 01
+Author: Nathan Rand
+Date: 12.05.2024
+"""
+
+_INPUT_FILE_NAME = "example.txt"
+
+def main():
+    """Advent of Code - Day 05 - Part 01 [PROBLEM NAME GOES HERE]"""
+    # Read in our input file
+    with open(_INPUT_FILE_NAME, "r", encoding="utf-8") as f:
+        input = f.read().split("\n")
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/template/problem1.py
@@ -0,0 +1,18 @@
+"""
+Advent of Code 2024
+Day: [day]
+Problem: 01
+Author: Nathan Rand
+Date: [date]
+"""
+
+_INPUT_FILE_NAME = "example.txt"
+
+def main():
+    """Advent of Code - Day [Insert Day Here] - Part 01 [PROBLEM NAME GOES HERE]"""
+    # Read in our input file
+    with open(_INPUT_FILE_NAME, "r", encoding="utf-8") as f:
+        input = f.read().split("\n")
+
+if __name__ == "__main__":
+    main()
