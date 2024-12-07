df435741a844f310e2a249b4c8b85b2985cdee89
Nathan Rand <nsrand02@gmail.com>
2024-12-07 08:37:00 -0600

    Day 7 setup

---
+++ 2024/day_07/problem1.py
@@ -0,0 +1,20 @@
+"""
+Advent of Code 2024
+Day: 07
+Problem: 01
+Author: Nathan Rand
+Date: 12.07.24
+"""
+
+_INPUT_FILE_NAME = "example.txt"
+
+
+def main():
+    """Advent of Code - Day 07 - Part 01 [Bridge Repair]"""
+    # Read in our input file
+    with open(_INPUT_FILE_NAME, "r", encoding="utf-8") as f:
+        input = f.read().split("\n")
+
+
+if __name__ == "__main__":
+    main()
