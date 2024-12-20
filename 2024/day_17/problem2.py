45e1be0c12fd3bc0c50f56368d11de7e6d50edf2
Nathan Rand <nsrand02@gmail.com>
2024-12-19 20:08:47 -0600

    I couldn't do it :(

--- 2024/day_17/problem2.py
+++ 2024/day_17/problem2.py
@@ -8,81 +8,17 @@ Date: 12.18.24
 import re
 _INPUT_FILE_NAME = "corrupted_example.txt"
 
-_REGISTERS = ["A", "B", "C"]
 
+def _num_to_base(num: int, base: int):
+    if num == 0:
+        return [0]
 
-def _combo_operand(operand: int, registers: dict):
-    """Apply combo operand logic."""
-    if operand == 7:
-        raise ValueError(
-            "Tried to do a combo operand on reserved operand [7]."
-        )
+    num_in_base = []
+    while num != 0:
+        num_in_base.append(num % base)
+        num //= base
 
-    return operand if operand <= 3 else registers[_REGISTERS[operand-4]]
-
-
-def _adv_instruction(operand: int, registers: dict):
-    """Instruction for opcode 0"""
-    registers["A"] = int(
-        registers["A"]/2**(_combo_operand(operand, registers))
-    )
-    return None
-
-
-def _bxl_instruction(operand: int, registers: dict):
-    """Instruction for opcode 1"""
-    registers["B"] = registers["B"] ^ operand
-    return None
-
-
-def _bst_instruction(operand: int, registers: dict):
-    """Instruction for opcode 2"""
-    registers["B"] = _combo_operand(operand, registers) % 8
-    return None
-
-
-def _jnz_instruction(operand: int, registers: dict):
-    """Instruction for opcode 3"""
-    return None if registers["A"] == 0 else operand
-
-
-def _bxc_instruction(operand: int, registers: dict):
-    """Instruction for opcode 4"""
-    registers["B"] = registers["B"] ^ registers["C"]
-    return None
-
-
-def _out_instruction(operand: int, registers: dict):
-    """Instruction for opcode 5"""
-    return _combo_operand(operand, registers) % 8
-
-
-def _bdv_instruction(operand: int, registers: dict):
-    """Instruction for opcode 6"""
-    registers["B"] = int(
-        registers["A"]/2**(_combo_operand(operand, registers))
-    )
-    return None
-
-
-def _cdv_instruction(operand: int, registers: dict):
-    """Instruction for opcode 7"""
-    registers["C"] = int(
-        registers["A"]/2**(_combo_operand(operand, registers))
-    )
-    return None
-
-
-_INSTRUCTIONS = [
-    _adv_instruction,
-    _bxl_instruction,
-    _bst_instruction,
-    _jnz_instruction,
-    _bxc_instruction,
-    _out_instruction,
-    _bdv_instruction,
-    _cdv_instruction
-]
+    return num_in_base
 
 
 def main():
@@ -100,61 +36,25 @@ def main():
 
     # NOTE: Going to brute force this first cause I am lazy
     solution_not_found = True
-    registers["A"] = 0
+    A = 8**(len(program)-1)
     while solution_not_found:
-        # Update our registry trying again with a new value for A
-        registers["A"] += 1
+        # Update our registry trying again with a new value for A)
+        A += 8
 
-        # Go through our programs performing the instructed actions
-        instruction_ptr = 0
-        program_output = []
-        invalid = True
-        while instruction_ptr < len(program)-1 and invalid:
-            # Grab our opcode and operand based on the position of the instruction ptr
-            opcode, operand = program[instruction_ptr:instruction_ptr+2]
-
-            # Get the instruction corresponding to this opcode and run it
-            opcode_instruction = _INSTRUCTIONS[opcode]
-            instruction_output = opcode_instruction(operand, registers)
-
-            # Mutate stuff in our program space based on the output if we have certain op codes
-            jumped = False
-            if opcode == 3:  # jnz instruction, sets ptr
-                if instruction_output is not None:
-                    instruction_ptr = instruction_output
-                    jumped = True
-            elif opcode == 5:  # out instruction, adds output to our program
-                program_output.append(instruction_output)
-
-            # Advance the instruction ptr if we did not jump
-            if not jumped:
-                instruction_ptr += 2
-
-            if program_output:
-                for i, out in enumerate(program_output):
-                    if out != program[i]:
-                        invalid = True
-                        break
-
-        if not invalid:
-            if len(program_output) != program:
-                continue
-
-            for i, out in enumerate(program):
-                if out != program_output[i]:
+        curr_output = _num_to_base(A//8, 8) + [0]
+        invalid = False
+        if len(program) > len(curr_output):
+            continue
+        else:
+            for expected, computed in zip(program, curr_output):
+                if expected != computed:
                     invalid = True
                     break
 
-        solution_not_found = invalid
+            solution_not_found = invalid
 
-    # Form our program output string
-    program_output_str = ",".join([str(num) for num in program_output])
-    print(registers["A"])
     # Output our result
-    print(
-        "The resulting output once the program halts is: "
-        f"{program_output_str}"
-    )
+    print(f"Register A should be: {A}")
 
 
 if __name__ == "__main__":
