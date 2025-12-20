45b0e19cd52c4213cb701ea15451e32b349afcfd
Nathan Rand <nsrand02@gmail.com>
2025-12-20 12:24:05 -0700

    Day 03 Part 01

---
+++ 2025/day_03/src/main.rs
@@ -0,0 +1,74 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+struct BatteryBank(String);
+
+impl BatteryBank {
+    fn get_max_joltage_for_bank(&self) -> anyhow::Result<u32> {
+        let batteries = self.0.chars();
+        let mut max_joltage = (0, 0);
+        for (idx, battery) in batteries.enumerate() {
+            // Cast our battery char as a digit
+            let battery_value = battery
+                .to_digit(10)
+                .unwrap_or_else(|| panic!("failed to cast battery: {battery} as digit"));
+
+            // If our battery is greater than our value in the tens place
+            // replace it and set the ones spot to zero
+            //
+            // NOTE: only do this if this is NOT the last number, otherwise it won't work
+            // as there is no digit to fill into the ones place
+            if battery_value > max_joltage.0 && idx != self.0.len() - 1 {
+                max_joltage = (battery_value, 0)
+            // Otherwise, if this is greater than our current max one digit, replace only that
+            } else if battery_value > max_joltage.1 {
+                max_joltage.1 = battery_value
+            }
+        }
+
+        Ok(max_joltage.0 * 10 + max_joltage.1)
+    }
+}
+
+/// Read in battery banks from disk
+fn read_battery_banks_from_disk(file_path: &str) -> anyhow::Result<Vec<BatteryBank>> {
+    let input = File::open(file_path)?;
+    let buffered = BufReader::new(input);
+    buffered
+        .lines()
+        .map(|line| Ok(BatteryBank(line?)))
+        .collect()
+}
+
+/// Part 1
+fn part_1() -> anyhow::Result<()> {
+    // Load in battery banks
+    let battery_banks = read_battery_banks_from_disk("data/input.txt")?;
+
+    // Compute the max joltage for each bank and sum together
+    let total_output_joltage =
+        battery_banks
+            .into_iter()
+            .try_fold(0, |acc, battery_bank| -> anyhow::Result<u32> {
+                Ok(acc + battery_bank.get_max_joltage_for_bank()?)
+            })?;
+
+    // Output result
+    println!("[Part 1] Total Output Joltage: {total_output_joltage} jolts");
+    Ok(())
+}
+
+/// Part 2
+fn _part_2() -> anyhow::Result<()> {
+    let _input = read_battery_banks_from_disk("data/example.txt")?;
+    todo!("Part 2!");
+}
+
+/// Main runner template
+fn main() -> anyhow::Result<()> {
+    part_1()?;
+    // part_2()?;
+    Ok(())
+}
