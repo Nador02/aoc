35f716762f1b7d2f11bf754884676c18ddf7c98e
Nathan Rand <nsrand02@gmail.com>
2025-12-24 10:18:22 -0500

    Day 4 Part 2 complete yay

--- 2025/day_04/src/main.rs
+++ 2025/day_04/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use itertools::Itertools;
 use std::{
     collections::HashSet,
@@ -77,8 +78,28 @@ impl PaperRollsDiagram {
         })
     }
 
+    pub fn point_to_index(&self, point: Point) -> usize {
+        self.width * (point.i as usize) + (point.j as usize)
+    }
+
     pub fn get_paper_roll(&self, point: Point) -> char {
-        self.grid[self.width * (point.i as usize) + (point.j as usize)]
+        self.grid[self.point_to_index(point)]
+    }
+
+    pub fn into_removed_rolls_at_points(mut self, points: HashSet<Point>) -> anyhow::Result<Self> {
+        for point in points {
+            if self.get_paper_roll(point) != '@' {
+                return Err(anyhow!(
+                    "didn't find paper roll at point: {point} found \'{}\'",
+                    self.get_paper_roll(point)
+                ));
+            }
+
+            let idx = self.point_to_index(point);
+            self.grid[idx] = '.';
+        }
+
+        Ok(self)
     }
 
     pub fn get_all_paper_roll_points(&self) -> HashSet<Point> {
@@ -120,13 +141,40 @@ fn part_1() -> anyhow::Result<()> {
 }
 
 /// Part 2
-fn _part_2() -> anyhow::Result<()> {
-    todo!("Part 2!");
+fn part_2() -> anyhow::Result<()> {
+    let mut paper_rolls_diagram = PaperRollsDiagram::from_disk("data/input.txt")?;
+
+    let mut num_forklift_accessible_rolls = 0;
+    loop {
+        let mut removable_rolls = HashSet::new();
+        let paper_roll_points = paper_rolls_diagram.get_all_paper_roll_points();
+
+        for point in &paper_roll_points {
+            let neighboring_points = point.get_neighboring_points();
+            let neighboring_paper_rolls: HashSet<Point> = neighboring_points
+                .intersection(&paper_roll_points)
+                .copied()
+                .collect();
+
+            if neighboring_paper_rolls.len() < 4 {
+                num_forklift_accessible_rolls += 1;
+                removable_rolls.insert(*point);
+            }
+        }
+
+        if removable_rolls.is_empty() {
+            break;
+        }
+        paper_rolls_diagram = paper_rolls_diagram.into_removed_rolls_at_points(removable_rolls)?;
+    }
+
+    println!("[Part 2] Total Number of Forklift Accessible Rolls: {num_forklift_accessible_rolls}");
+    Ok(())
 }
 
 /// Main runner template
 fn main() -> anyhow::Result<()> {
     part_1()?;
-    // part_2()?;
+    part_2()?;
     Ok(())
 }
