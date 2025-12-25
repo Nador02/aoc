56e62da1afb6c8cb2ba3d463fe1bcd03b1c33d3f
Nathan Rand <nsrand02@gmail.com>
2025-12-25 08:17:35 -0500

    Day 5 Part 1

--- 2025/day_05/src/main.rs
+++ 2025/day_05/src/main.rs
@@ -1,8 +1,35 @@
 use std::{
-    collections::HashSet, fs::File, io::{BufRead, BufReader}, process::id
+    collections::HashSet,
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+    ops::RangeInclusive,
 };
 
+use itertools::Itertools;
+
+struct IngredientIDRanges(Vec<RangeInclusive<usize>>);
+
 struct IngredientIDs(HashSet<usize>);
+
+impl Display for IngredientIDs {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]", self.0.iter().join(","))
+    }
+}
+
+impl Display for IngredientIDRanges {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "[{}]",
+            self.0
+                .iter()
+                .map(|range| format!("({}, {})", range.start(), range.end()))
+                .join(",")
+        )
+    }
+}
 
 impl IngredientIDs {
     pub fn new() -> Self {
@@ -14,12 +41,34 @@ impl IngredientIDs {
     }
 }
 
+impl IngredientIDRanges {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn push(&mut self, ingredient_id_range: RangeInclusive<usize>) {
+        self.0.push(ingredient_id_range)
+    }
+
+    pub fn contains(&self, ingredient_id: &usize) -> bool {
+        for range in &self.0 {
+            if range.contains(ingredient_id) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
 /// Read in input
-fn get_fresh_and_available_ingredient_ids_from_disk(file_path: &str) -> anyhow::Result<(IngredientIDs, IngredientIDs)> {
+fn get_fresh_and_available_ingredient_ids_from_disk(
+    file_path: &str,
+) -> anyhow::Result<(IngredientIDRanges, IngredientIDs)> {
     let input = File::open(file_path)?;
     let buffered = BufReader::new(input);
 
-    let mut fresh_ingredient_ids = IngredientIDs::new();
+    let mut fresh_ingredient_ids = IngredientIDRanges::new();
     let mut avaialable_ingredient_ids = IngredientIDs::new();
     let mut found_input_split = false;
 
@@ -29,8 +78,18 @@ fn get_fresh_and_available_ingredient_ids_from_disk(file_path: &str) -> anyhow::
         if found_input_split {
             avaialable_ingredient_ids.insert(line?.parse::<usize>()?);
         } else {
+            // Check if this is where we should split
+            let line_str = line?;
+            if line_str.is_empty() {
+                found_input_split = true;
+                continue;
+            }
+
             // Otherwise, continue adding ranges to our fresh ingredient IDs
-            fresh_ingredient_ids.insert(line?.parse::<usize>()?);
+            let (id_range_start, id_range_end) = line_str.split_once("-").unwrap();
+            let id_range_start = id_range_start.parse::<usize>()?;
+            let id_range_end = id_range_end.parse::<usize>()?;
+            fresh_ingredient_ids.push(RangeInclusive::new(id_range_start, id_range_end));
         }
     }
 
@@ -39,8 +98,16 @@ fn get_fresh_and_available_ingredient_ids_from_disk(file_path: &str) -> anyhow::
 
 /// Part 1
 fn part_1() -> anyhow::Result<()> {
-    let (fresh_ingredient_ids, avaialble_ingredient_ids) = get_fresh_and_available_ingredient_ids_from_disk("data/input.txt")?;
-    todo!("Part 1!");
+    let (fresh_ingredient_ids, available_ingredient_ids) =
+        get_fresh_and_available_ingredient_ids_from_disk("data/input.txt")?;
+    let num_fresh_ingredients = available_ingredient_ids
+        .0
+        .iter()
+        .filter(|&ingredient_id| fresh_ingredient_ids.contains(ingredient_id))
+        .collect::<Vec<&usize>>()
+        .len();
+    println!("[Part 1]: Number of Fresh Ingredients Available: {num_fresh_ingredients}");
+    Ok(())
 }
 
 /// Part 2
@@ -52,6 +119,6 @@ fn part_2() -> anyhow::Result<()> {
 /// Main runner template
 fn main() -> anyhow::Result<()> {
     part_1()?;
-    part_2()?;
+    // part_2()?;
     Ok(())
 }
